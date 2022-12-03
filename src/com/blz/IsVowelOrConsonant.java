a68063fe9a372d63a03913fd86d2389e917fbfc4
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-03 15:26:59 +0530

    Added : Whether an Alphabet is Vowel or Consonant

---
+++ src/com/blz/IsVowelOrConsonant.java
@@ -0,0 +1,29 @@
+package com.blz;
+
+import java.util.Scanner;
+
+public class IsVowelOrConsonant {
+    public static void main(String[] args) {
+        System.out.println("Enter A Alphabet");
+        Scanner sc = new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+
+        switch(ch) {
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+            case "A":
+            case "E":
+            case "I":
+            case "O":
+            case "U":
+
+                System.out.println("vowel");
+                break;
+            default:
+                System.out.println("consonant");
+        }
+    }
+}
